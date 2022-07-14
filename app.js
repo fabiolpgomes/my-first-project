c66a7316b0fc3066c4e4bae864c09c080386faf9
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-07-14 02:39:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Declare variable: let const var picturespokemon
+let count = 25
+
+
+function render(){
+    let container = document.getElementById('picturespokemon')
+container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`
+}
+
+let prev = document.getElementById('prev')
+let next = document.getElementById('next')
+
+prev.onclick = function(){
+    if (count > 1) {
+        count = count -1
+        render() 
+    }
+    
+}
+
+next.onclick = function(){
+    if (count < 650) {
+        count = count +1
+        render() 
+    }
+}
+render()
+
+
+
